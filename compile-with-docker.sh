326eb06f6fd60125ad9762ceb75bea6d4bf5a49e
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2024-10-21 12:22:00 +0200

    compile-with-docker.sh generalized and clearer

--- compile-with-docker.sh
+++ compile-with-docker.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #export DOCKER_DEFAULT_PLATFORM=linux/amd64
 IMAGE_NAME="uvk5"
+DEST_DIR="compiled-firmware"
 docker build -t $IMAGE_NAME .
-docker run --rm -v "${PWD}/compiled-firmware:/app/compiled-firmware" $IMAGE_NAME /bin/bash -c "rm ./compiled-firmware/*; cd /app && make && cp f4hwn* compiled-firmware/"
+docker run --rm -v "${PWD}/${DEST_DIR}:/app/${DEST_DIR}" $IMAGE_NAME \
+  /bin/bash -c "rm ./${DEST_DIR}/*; cd /app && make && cp f4hwn* ${DEST_DIR}/"
